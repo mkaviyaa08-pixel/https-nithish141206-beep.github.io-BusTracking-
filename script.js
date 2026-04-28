749eefafde237be1b358eb5ff8eb82dc8871a443
mkaviyaa08-pixel <mkaviyaa08@gmail.com>
2026-04-29 00:05:16 +0530

    Update and rename logi.html to script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function login() {
+    var user = document.getElementById("username").value;
+    var pass = document.getElementById("password").value;
+
+    if(user === "admin" && pass === "1234") {
+        window.location.href = "index.html";
+    } else {
+        document.getElementById("error").innerHTML = "Enter valid username/password";
+    }
+}
